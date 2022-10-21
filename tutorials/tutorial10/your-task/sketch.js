1cf0ae47b2856cf5d51d61776fab6c82fb5b408a
Julia190 <jobwills03@gmail.com>
2022-10-21 19:21:40 -0400

    TUTORIAL10ADDDDDDDDD

--- tutorials/tutorial10/your-task/sketch.js
+++ tutorials/tutorial10/your-task/sketch.js
@@ -1,24 +1,99 @@
 const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
-
     
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
-
-    drawCreature(425, 225, 200);
-
     drawGrid(canvasWidth, canvasHeight);
 }
-
-// replace this function with YOUR creature!
-function drawCreature(x, y, width, primaryColor='hotpink', secondaryColor='black') {
+function drawCreature(x, y, size, primaryColor, secondaryColor ) {
     fill(primaryColor);
-    stroke(secondaryColor);
-    circle(x, y, width);
+    ellipse(x-(size*.33),y-(size*.66), size*.33, size);
+    
+    fill(secondaryColor);
+    ellipse(x-(size*.33), y-(size*.66), size*.233, size*.66);
 
-    noStroke();
+    fill(primaryColor);
+    ellipse(x+(size*.33),y-(size*.66), size*.33, size);
+
+    fill(secondaryColor);
+    ellipse(x+(size*.33), y-(size*.66), size*.233, size*.66);
+
+    fill(primaryColor);
+    circle(x, y, size);
+
+    fill(secondaryColor);
+    ellipse(x, y+(size*.22), size*.233, size*.066);
+
     fill('black');
-    textSize(16);
-    textAlign(CENTER);
-    text("Replace with your creature", x, y);
+    circle(x+(size*.2), y, size*.1);
+
+    fill('black');
+    circle(x-(size*.2), y, size*.1);
 }
+
+const bubbles = [];
+let x = canvasWidth / 2;
+let y = canvasHeight / 2 - 50;
+let d = 50;
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+
+   drawCreature(x, y, 50, 'lightgray', 'lightpink')
+
+    displayMessage();
+}
+
+function shoot() {
+    // 1. When you drag your mouse, create a bubble 
+    //    object and add it to the "bubbles" list.
+    const bubble = {
+        x: x,
+        y: y - 25,
+        d: 8,
+        speed: 10
+    }
+    bubbles.push(bubble)
+}
+
+function draw() {
+    clear();
+    // 2. loop through the bubbles list and draw / animate each bubble:
+    for (const bubble of bubbles) {
+        circle(bubble.x, bubble.y, bubble.d);
+        bubble.y -= bubble.speed;
+    }
+
+    drawCreature(x, y, 50, 'lightgray', 'lightpink')
+
+    // message:
+    displayMessage();
+
+}
+
+
+function moveController(ev) {
+    console.log(ev.code);
+    if (ev.code == 'ArrowLeft') {
+        console.log('left arrow!');
+        x -= 5;
+    } else if (ev.code == 'ArrowRight') {
+        console.log('right arrow!');
+        x += 5;
+    } else if (ev.code == 'Space') {
+        shoot();
+    }
+    
+    // message
+   displayMessage();
+}
+
+function displayMessage() {
+    fill('black');
+     textAlign(CENTER);
+     textSize(16);
+     text("Use the arrow keys to move the Bunny, spacebar to shoot.", canvasWidth/2, canvasHeight/2);
+}
+
+// Add event listener on keydown
+document.addEventListener('keydown', moveController);
