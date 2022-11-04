0b1823e5bf38375ba3976fe63021e6fb5330ad68
Julia190 <jobwills03@gmail.com>
2022-11-04 13:45:01 -0400

    tutorial 12 quiz add

---
+++ tutorials/tutorial12/exercise01/main.js
@@ -0,0 +1,24 @@
+function makeRed() {
+    // your code here...
+    console.log('Change the box\'s background color to red');
+    document.querySelector('section').style.backgroundColor = 'red';
+}
+
+function makeBlue() {
+    // your code here...
+    console.log('Change the box\'s background color to blue');
+    document.querySelector('section').style.backgroundColor = 'blue';
+}
+
+function makePink() {
+    // your code here...
+    console.log('Change the box\'s background color to pink');
+    document.querySelector('section').style.backgroundColor = 'pink';
+}
+
+function makeOrange() {
+    // your code here...
+    console.log('Change the box\'s background color to orange');
+    document.querySelector('section').style.backgroundColor = 'orange';
+}
+
